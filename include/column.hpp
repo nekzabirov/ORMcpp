a7e9fa19cdc77e817cbdbee68537d0220d7b7836
nekzabirov <neka1dev@gmail.com>
2025-03-24 14:55:57 +0200

    Refactor `Column` class and improve optional handling
    This update restructures the `Column` class for better readability and introduces improved handling for `std::optional` values. It also standardizes code formatting and ensures consistency in template operations.

--- include/column.hpp
+++ include/column.hpp
@@ -12,106 +12,137 @@
 #include "sql_stream.hpp"
 #include "conditional.hpp"
 
-namespace nek::sql {
-    class Column final : public SqlStream {
+namespace nek::sql
+{
+    class Column final : public SqlStream
+    {
     public:
-        explicit Column(const std::string_view &name);
+        explicit Column(const std::string_view& name);
 
-        Column(const Column &other): SqlStream(other.str()) {
+        Column(const Column& other): SqlStream(other.str())
+        {
         }
 
-        template<typename T, std::enable_if_t<!std::is_same_v<T, Column>, int>  = 0>
-        Column &&operator=(const T &value) {
+        template <typename T, std::enable_if_t<!std::is_same_v<T, Column>, int>  = 0>
+        Column&& operator=(const T& value)
+        {
             append(key);
             append(" = ");
             append(formatValue(value));
             return std::move(*this);
         }
 
-        template<typename T, std::enable_if_t<!std::is_same_v<T, Column>, int>  = 0>
-        Conditional operator==(const T &value) {
+        template <typename T, std::enable_if_t<!std::is_same_v<T, Column>, int>  = 0>
+        Column&& operator=(const std::optional<T>& value)
+        {
+            if (!value)
+            {
+                return std::move(*this);
+            }
+
+            append(key);
+            append(" = ");
+            append(formatValue(value));
+            return std::move(*this);
+        }
+
+        template <typename T, std::enable_if_t<!std::is_same_v<T, Column>, int>  = 0>
+        Conditional operator==(const T& value)
+        {
             return Conditional(std::format("{} = {}", key, formatValue(value)));
         }
 
-        template<typename T, std::enable_if_t<!std::is_same_v<T, Column>, int>  = 0>
-        Conditional operator==(const std::optional<T> &value) {
+        template <typename T, std::enable_if_t<!std::is_same_v<T, Column>, int>  = 0>
+        Conditional operator==(const std::optional<T>& value)
+        {
             return Conditional(value ? std::format("{} = {}", key, formatValue(value.value())) : "");
         }
 
-        template<typename T, std::enable_if_t<!std::is_same_v<T, Column>, int>  = 0>
-        Conditional operator!=(const T &value) {
+        template <typename T, std::enable_if_t<!std::is_same_v<T, Column>, int>  = 0>
+        Conditional operator!=(const T& value)
+        {
             return Conditional(std::format("{} != {}", key, formatValue(value)));
         }
 
-        template<typename T, std::enable_if_t<!std::is_same_v<T, Column>, int>  = 0>
-        Conditional operator>(const T &value) {
+        template <typename T, std::enable_if_t<!std::is_same_v<T, Column>, int>  = 0>
+        Conditional operator>(const T& value)
+        {
             return Conditional(std::format("{} > {}", key, formatValue(value)));
         }
 
-        template<typename T, std::enable_if_t<!std::is_same_v<T, Column>, int>  = 0>
-        Conditional operator>=(const T &value) {
+        template <typename T, std::enable_if_t<!std::is_same_v<T, Column>, int>  = 0>
+        Conditional operator>=(const T& value)
+        {
             return Conditional(std::format("{} >= {}", key, formatValue(value)));
         }
 
-        template<typename T, std::enable_if_t<!std::is_same_v<T, Column>, int>  = 0>
-        Conditional operator<(const T &value) {
+        template <typename T, std::enable_if_t<!std::is_same_v<T, Column>, int>  = 0>
+        Conditional operator<(const T& value)
+        {
             return Conditional(std::format("{} < {}", key, formatValue(value)));
         }
 
-        template<typename T, std::enable_if_t<!std::is_same_v<T, Column>, int>  = 0>
-        Conditional operator<=(const T &value) {
+        template <typename T, std::enable_if_t<!std::is_same_v<T, Column>, int>  = 0>
+        Conditional operator<=(const T& value)
+        {
             return Conditional(std::format("{} < {}", key, formatValue(value)));
         }
 
-        template<typename T, std::enable_if_t<!std::is_same_v<T, Column>, int>  = 0>
-        Conditional like(const T &value) {
+        template <typename T, std::enable_if_t<!std::is_same_v<T, Column>, int>  = 0>
+        Conditional like(const T& value)
+        {
             return Conditional(std::format("{} LIKE {}", key, formatValue(value)));
         }
 
-        template<typename T, std::enable_if_t<!std::is_same_v<T, Column>, int>  = 0>
-        Conditional like(const std::optional<T> &value) {
+        template <typename T, std::enable_if_t<!std::is_same_v<T, Column>, int>  = 0>
+        Conditional like(const std::optional<T>& value)
+        {
             return Conditional(value ? std::format("{} LIKE {}", key, formatValue(value.value())) : "");
         }
 
-        template<class T>
-        Conditional in(const std::initializer_list<T> values) {
+        template <class T>
+        Conditional in(const std::initializer_list<T> values)
+        {
             std::ostringstream query;
 
             int index = 0;
             query << std::accumulate(values.begin(), values.end(), std::string{},
-                                     [&index](const std::string &acc, const T &value) {
+                                     [&index](const std::string& acc, const T& value)
+                                     {
                                          return acc + (index++ > 0 ? ", " : "") + formatValue(value);
                                      });
 
             return Conditional(std::format("{} IN ({})", key, query.str()));
         }
 
-        template<class T>
-        Conditional any(const std::initializer_list<T> values) {
+        template <class T>
+        Conditional any(const std::initializer_list<T> values)
+        {
             std::ostringstream query;
 
             int index = 0;
             query << std::accumulate(values.begin(), values.end(), std::string{},
-                                     [&index](const std::string &acc, const T &value) {
+                                     [&index](const std::string& acc, const T& value)
+                                     {
                                          return acc + (index++ > 0 ? ", " : "") + formatValue(value);
                                      });
 
             return Conditional(std::format("{} IN ({})", key, query.str()));
         }
 
-        Column &&operator=(const Column &value);
+        Column&& operator=(const Column& value);
 
-        Conditional operator==(const Column &value);
+        Conditional operator==(const Column& value);
 
-        Conditional operator!=(const Column &value);
+        Conditional operator!=(const Column& value);
 
-        Conditional operator>(const Column &value);
+        Conditional operator>(const Column& value);
 
-        Conditional operator>=(const Column &value);
+        Conditional operator>=(const Column& value);
 
-        Conditional operator<(const Column &value);
+        Conditional operator<(const Column& value);
 
-        Conditional operator<=(const Column &value);
+        Conditional operator<=(const Column& value);
 
     private:
         std::string_view key;
